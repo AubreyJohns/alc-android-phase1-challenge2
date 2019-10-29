f1833bf5fc172f7f8f13e1702a3e144709db949b
Aubrey J <aubreyomondi@gmail.com>
2019-10-29 09:52:11 +0300

    Created FirebaseUtil class

--- app/src/main/java/com/example/travelmantics_alc/FirebaseUtil.java
+++ app/src/main/java/com/example/travelmantics_alc/FirebaseUtil.java
@@ -1,5 +1,27 @@
 package com.example.travelmantics_alc;
 
-public class FirebaseUtil {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
+public class FirebaseUtil {
+    //static - to initiate members without any instance of class
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil firebaseUtil;
+    public static ArrayList<TravelDeal> mdeals;
+
+    // to avoid instantiation of this class from outside
+    private FirebaseUtil(){}
+
+    // open reference of child parameter
+    public static void openFbReference(String ref){
+        if (firebaseUtil == null){
+            firebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mdeals = new ArrayList<TravelDeal>();
+        }
+        mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
 }
